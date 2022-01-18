67e039a543a670951320e61cf6951b1219140d32
michael beloff <michael.a.beloff@gmail.com>
2022-01-18 12:31:44 +1000

    style tweaks

--- src/components/FormModal.vue
+++ src/components/FormModal.vue
@@ -5,20 +5,26 @@
     :focus-trap="true"
     :click-to-close="false"
     overlay-class="backdrop-blur-lg"
-    classes="flex justify-center items-center mx-auto overflow-y-auto h-screen"
-    content-class="absolute top-0 flex flex-col border bg-white p-2 my-2 rounded max-w-lg"
+    classes="flex justify-center items-center mx-auto overflow-y-auto absolute top-0 bottom-0 my-2"
+    content-class="absolute top-0 flex flex-col bg-white max-w-lg"
   >
-    <h2 class="text-xl font-medium leading-6 flex items-center justify-between text-gray-900">
-                Online Application
+  <div class="bg-orange-500 z-50 sticky w-full p-2 top-0 left-0">
+    <h2 class="text-xl font-bold leading-6 flex items-center justify-between text-white">
+      Online Application
       <button @click="$emit('cancel', close)"
         type="button"
-        class="inline-flex shadow-md shadow-orange-500/30 justify-center p-2 px-3 bg-orange-200 border border-transparent rounded-md hover:bg-orange-500 text-orange-500 hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+        class="inline-flex shadow-md justify-center p-2 px-3 bg-orange-200 border border-transparent rounded-md hover:bg-orange-400 text-orange-500 hover:text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
       >
         <i class="far fa-times"></i>
       </button>
     </h2>
+    <div class="h-12 bg-gradient-to-b pointer-events-none from-white absolute w-full left-0 top-full"></div>
+  </div>
+    
 
-    <div class="flex-grow">
+    
+
+    <div class="flex-grow p-2">
       <slot :params="params"></slot>
     </div>
   </vue-final-modal>
