44360996cd10989fc56f99d938329042bad92eca
michael beloff <michael.a.beloff@gmail.com>
2022-01-25 13:52:36 +1000

    add error modal

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       <form-apply></form-apply>
     </form-modal>
 
-    <vue-final-modal v-model="showConfirmation" name="confirmation" overlay-class="backdrop-blur-lg" :focus-trap="true" :click-to-close="false" :transition="{
+    <vue-final-modal v-model="showConfirmation" name="dialog-confirmation" overlay-class="backdrop-blur-lg" :focus-trap="true" :click-to-close="false" :transition="{
         'enter-active-class': 'transition duration-200 ease-in-out transform',
         'enter-from-class': 'translate-y-full',
         'enter-to-class': 'translate-y-0',
@@ -23,18 +23,34 @@
       }">
       <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-2xl flex flex-col p-4 gap-2 w-full max-w-md text-center">
 
-        <div class="mx-auto flex items-center justify-center h-24 w-24 rounded-full bg-green-100">
-          <svg class="h-6 w-6 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-          </svg>
+        <div class="-mt-10 mx-auto flex items-center justify-center h-24 w-24 rounded-full text-green-400 bg-green-100">
+          <i class="far fa-check fa-2x inline-block mt-1"></i>       
         </div>
         <p class="text-xl font-bold leading-6 text-amber-500">Application Received</p>
         <p>We will be in touch shortly</p>
         <button @click.prevent="closeAll" class="bg-amber-500 shadow-lg shadow-amber-500/30 py-2 transition duration-500 hover:-translate-y-1 ease-out text-white rounded hover:bg-amber-400 px-6 w-max mx-auto">Okay</button>
       </div>
     </vue-final-modal>
+   
+    <vue-final-modal v-model="showError" name="dialog-error" overlay-class="backdrop-blur-lg" :focus-trap="true" :click-to-close="false" :transition="{
+        'enter-active-class': 'transition duration-200 ease-in-out transform',
+        'enter-from-class': 'translate-y-full',
+        'enter-to-class': 'translate-y-0',
+        'leave-active-class': 'transition duration-200 ease-in-out transform',
+        'leave-to-class': 'translate-y-full',
+        'leave-from-class': 'translate-y-0'
+      }">
+      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg shadow-2xl flex flex-col p-4 gap-2 w-full max-w-md text-center">
+        <div class="-mt-10 mx-auto flex items-center justify-center h-24 w-24 rounded-full text-red-300 bg-red-100">
+          <i class="far fa-times fa-2x inline-block mt-1"></i>       
+        </div>
+        <p class="text-xl font-bold leading-6 text-amber-500">Something went wrong</p>
+        <p>Please try again, and contact us if the error persists.</p>
+        <button @click.prevent="closeError" class="bg-amber-500 shadow-lg shadow-amber-500/30 py-2 transition duration-500 hover:-translate-y-1 ease-out text-white rounded hover:bg-amber-400 px-6 w-max mx-auto">Okay</button>
+      </div>
+    </vue-final-modal>
 
-    <main class="pt-20">
+    <main class="mt-20">
       <router-view />
     </main>
 
@@ -58,6 +74,7 @@
     data: () => ({
       show: false,
       showConfirmation: false,
+      showError: false,
     }),
     methods: {
       cancel(close) {
@@ -65,14 +82,15 @@
         close()
       },
       closeAll() {
-        // this.$vfm.hide('confirmation').then(this.$vfm.hide('form'))
         this.$vfm.hide('form')
-        // this.$vfm.hide('confimation')
         setTimeout(this.closeConfirmation, 500)
         
       },
       closeConfirmation() {
-        this.$vfm.hide('confirmation')
+        this.$vfm.hide('dialog-confirmation')
+      },
+      closeError() {
+        this.$vfm.hide('dialog-error')
       }
     },
     watch: {
